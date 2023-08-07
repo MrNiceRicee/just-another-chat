429be82906ad6a24b20565dbda803d026a7ffdfe
Joshua Santos <JoshCSan@gmail.com>
2023-08-06 18:11:57 -0700

    - migrate: pages to apps

---
+++ apps/nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-stone-200">
+      <h1 className="text-4xl text-zinc-900">
+        Hey!
+        <span>👋</span>
+      </h1>
+    </div>
+  );
+}
